30458c32d1ba5d31cee0c699face64e830662426
samueldlay <22435185+samueldlay@users.noreply.github.com>
2024-01-14 14:23:36 -0700

    styling

---
+++ src/components/Heading.tsx
@@ -0,0 +1,5 @@
+export default function Heading() {
+  return (
+    <h1 className="self-center">Welcome</h1>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./components/App.tsx";
 import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </StrictMode>,
 );
